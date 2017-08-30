9af842df2a13e8fb7229b6f0cbe0c9ed1f6969b0
Sascha Weidner <support@sioweb.de>
2017-08-30 08:08:22 +0200

    CSS/JS im Plugin nutzen

--- bundles/DummyBundle/src/Resources/contao/config/config.php
+++ bundles/DummyBundle/src/Resources/contao/config/config.php
@@ -12,6 +12,15 @@
  * @copyright Sioweb, Sascha Weidner
  */
 
+
+if(TL_MODE == 'FE') {
+	// Pfad ggf. anpassen
+	// Alle Dateien in /src/Ressources/public werden unter /web/bundles/bundle-name
+	// als Symlink veröffentlicht nach composer install/update
+	$GLOBALS['TL_CSS'][] = 'web/bundles/contao4dummy/css/dummy.css|static';
+	$GLOBALS['TL_JAVASCRIPT'][] = 'web/bundles/contao4dummy/js/dummy.js|static';
+}
+
 array_insert($GLOBALS['TL_CTE']['texts'],2,array (
 	'content_dummy' => 'Sioweb\ContentDummy',
 ));
