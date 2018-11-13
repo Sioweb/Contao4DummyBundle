51f6eae563eb8208d17d434122b63f50ac55e48a
Sascha Weidner <=>
2018-11-13 11:30:46 +0100

    Fix assets path

--- bundles/Sioweb/DummyBundle/src/EventListener/System.php
+++ bundles/Sioweb/DummyBundle/src/EventListener/System.php
@@ -29,7 +29,7 @@ class system {
             // Alle Dateien in /src/Ressources/public werden unter /web/bundles/bundle-name
             // als Symlink veröffentlicht nach composer install/update
             $GLOBALS['TL_CSS'][] = 'bundles/dummybundle/css/dummy.css|static';
-            $GLOBALS['TL_JAVASCRIPT'][] = 'bundles/contao4dummy/js/dummy.js|static';
+            $GLOBALS['TL_JAVASCRIPT'][] = 'bundles/dummybundle/js/dummy.js|static';
         }
         
         array_insert($GLOBALS['TL_CTE']['texts'],2,array (
