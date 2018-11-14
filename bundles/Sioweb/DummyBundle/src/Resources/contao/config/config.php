a3e57a51a8fdd53d2ee60f68ac0b25d8d6a3dbf4
Sascha Weidner <=>
2018-11-14 09:54:32 +0100

    Add config to config.php because 4.4 has no tag

--- bundles/Sioweb/DummyBundle/src/Resources/contao/config/config.php
+++ bundles/Sioweb/DummyBundle/src/Resources/contao/config/config.php
@@ -11,5 +11,21 @@
  * @copyright Sioweb, Sascha Weidner
  */
 
- // Achtung: Die Config befindet sich nun in der Klasse DummyBundle/src/EventListener/System
- // Der "Hook", der die Klasse aufruft, ist in DummyBundle/src/Resoureces/config/listener.yml definiert
+// Achtung:
+// Diese Datei ist nur in Version 4.4 nötig, aber 4.5 kann die Datei weggelassen werden.
+// Die Config befindet sich dann in der Klasse DummyBundle/src/EventListener/System
+// Der "Hook", der die Klasse aufruft, ist in DummyBundle/src/Resoureces/config/listener.yml definiert
+
+if(VERSION <= 4.5) {
+    if (TL_MODE === 'FE') { // Früher TL_MODE == 'FE'
+        // Pfad ggf. anpassen
+        // Alle Dateien in /src/Ressources/public werden unter /web/bundles/bundle-name
+        // als Symlink veröffentlicht nach composer install/update
+        $GLOBALS['TL_CSS'][] = 'bundles/dummybundle/css/dummy.css';
+        $GLOBALS['TL_JAVASCRIPT'][] = 'bundles/dummybundle/js/dummy.js';
+    }
+
+    array_insert($GLOBALS['TL_CTE']['texts'], 2, array(
+        'content_dummy' => 'Sioweb\ContentElement\ContentDummy',
+    ));
+}
